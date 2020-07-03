5e5ba7cd44dbfc639f61309163babfd1a1583364
pankajkoirala <pankaj.koirala.77@gmail.com>
2020-07-03 20:06:00 +0545

    edit

--- src/utility/validator/formValidator.js
+++ src/utility/validator/formValidator.js
@@ -13,31 +13,25 @@ export const personDetailValidator = Yup.object().shape({
     .min(2, "Too Short!")
     .max(50, "Too Long!")
     .required("Required"),
-  age: Yup.number()
+  age: Yup.number().integer().positive()
     .min(2, "Too Short!")
     .max(150, "Too Long!")
     .required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
   gender: Yup.string().oneOf(["male", "female", "other"]).required("Required"),
-  phone: Yup.number()
+  phone: Yup.number().integer().positive()
     .min(999999999, "Too Short!")
     .max(9999999999, "Too Long!")
     .required("Required"),
-  date:  Yup.date()
+  date:  Yup.date().integer().positive()
   .required('Required'),
   
   country: Yup.string().max(50, "Too Long!").required("Required"),
-  city: Yup.string().max(50, "Too Long!").required("Required"),
+  city: Yup.string() .max(50, "Too Long!").required("Required"),
   state: Yup.string().oneOf(["1","2","3","4","5","6","7"]).required("Required"),
-  zip: Yup.number()
+  zip: Yup.number().integer().positive()
     .max(99999, "Too Long!")
     .required("Required"),
 });
 
-/* 
-phone: "",
-date: "",
-country: "",
-city: "",
-state: "",
-zip: "",*/
+
